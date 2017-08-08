89ecdfb164a34d4fb76333ea5cb57cb999a28527
Victor Soto <jvictorsotom@gmail.com>
2017-08-08 17:07:16 +0200

    feat(lib): Addapted to angular-material

--- src/ng-prettyjson-tmpl.js
+++ src/ng-prettyjson-tmpl.js
@@ -4,12 +4,14 @@
   angular.module('ngPrettyJson')
   .run(['$templateCache', function ($templateCache) {
     $templateCache.put('ng-prettyjson/ng-prettyjson-panel.tmpl.html',
-    '<div>' + 
-    '<button type="button" ng-click="edit()" ng-show="edition && !editActivated">Edit</button>' +
-    '<button type="button" ng-click="edit()" ng-show="edition && editActivated">Cancel</button>' +
-    '<button type="button" ng-click="update()" ng-show="editActivated && parsable">Update</button>' +
-    '<pre class="pretty-json ace_editor ace-tm" id="{{id}}"></pre>' +                        
-    '</div>');
+    '<div>' +
+    '  <div layout="row" layout-align="end end">' + 
+    '    <md-button class="md-icon-button" ng-click="edit()" ng-show="edition && !editActivated"><md-icon class="material-icons">edit</md-icon></md-button>' +
+    '    <md-button class="md-icon-button" ng-click="edit()" ng-show="edition && editActivated"><md-icon class="material-icons">close</md-icon></md-button>' +
+    '    <md-button class="md-icon-button" ng-click="update()" ng-show="editActivated && parsable"><md-icon class="material-icons">save</md-icon></md-button>' +                       
+    '  </div>' +
+    '  <pre class="pretty-json ace_editor ace-tm" id="{{id}}"></pre>' +
+    '</div>' );
   }]);
 
 })(window.angular);
